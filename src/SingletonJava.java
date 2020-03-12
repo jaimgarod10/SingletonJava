279a9e4e4b7e4da09c3c96052200c62202d97eac
jaimgarod10 <jaimgarod@gmail.com>
2020-03-12 17:17:28 +0100

    Singleton en Java

---
+++ src/SingletonJava.java
@@ -0,0 +1,11 @@
+public class SingletonJava{
+
+public static void main(String[]args){
+
+    Persona obx1= Persona.getInstance("Pepe", 34, "Guerrero");
+    Persona obx2=Persona.getInstance("Roberto",36,"DosSantos");
+        System.out.println(obx1.getNombre());
+        System.out.println(obx2.getNombre());
+
+    }
+}
